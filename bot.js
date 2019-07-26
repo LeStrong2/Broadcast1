38d539d89da05507187bbbdd9e9513a7f7792319
LeStrong2 <53224657+LeStrong2@users.noreply.github.com>
2019-07-26 16:44:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,17 +53,6 @@ client.on('ready',  () => {
     console.log(`Logged in as * [ " ${client.user.username} " ] channels! [ " ${client.channels.size} " ]`);
   });
 
-  client.on('message', message => {
-    if(!message.channel.guild) return;
-let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('!adminbc')){
-if(!message.author.id === '560360141749354496') return;
-message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
-client.users.forEach(m =>{
-m.sendMessage(args)
-})
-}
-});
 
   client.on('message', msg => {
     if(msg.content === '!help')
