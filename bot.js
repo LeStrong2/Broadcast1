38876d1377019fb70bf85daef1e326cc90016a65
LeStrong2 <53224657+LeStrong2@users.noreply.github.com>
2020-02-03 16:31:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,21 +4,6 @@ var prefix = "#"
 var adminprefix = '#'
 
 
-//bc
-
-client.on("message", message => {
-    if (message.content.startsWith("#bc")) {
-                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' ');
-  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
-  m.send(`${argresult}\n ${m}`);
-  })
-  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
-  message.delete();
-  };
-  });
-
 
 //bc online
 
